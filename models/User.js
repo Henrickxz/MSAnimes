c369ead15a2d3e08599801bc430cc24815a54f6e
Henrickxz <145411314+Henrickxz@users.noreply.github.com>
2024-10-22 22:33:46 -0300

    finalizado

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("User", {
     email: String,
-    senha: String
+    senha: String,
+    idade: Number
 });
 
 module.exports = User;
